ca37101c74299678b5697f6655908282e6ac9e12
Dos Santos Hindjou <Dozzy7@yahoo.com>
2017-06-20 22:16:24 +0800

    agent panel for adding, editingand deleting properties

---
+++ agentpanel.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <?php include 'templates/head.php';?>
+
+    <body>
+
+      <!---------------- Header ---------------->
+     <?php include 'templates/header.php';?>
+
+    <!----------------  backgroundimage ---------------->
+    <?php include 'templates/backgroundimage.php';?>
+
+     <!----------------  ---------------->
+     <?php include 'templates/tab.php';?>
+     
+    <!---------------- footer ---------------->
+<?php include 'templates/footer.php';?>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
